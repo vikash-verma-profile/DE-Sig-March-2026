c098ace22db553d10905af66a32c431859bc1b96
vikash-verma-profile <er.vikashverma@yahoo.com>
2026-04-16 12:01:59 +0530

    added

--- Week-3/Day-3/labs/lab-2/main.tf
+++ Week-3/Day-3/labs/lab-2/main.tf
@@ -23,9 +23,14 @@ provider "azurerm" {
 # }
 
 resource "azurerm_virtual_network" "example" {
-  name                = "Vikash-network"
+  name                = "Vikash-network-New"
   resource_group_name = "Sigmoid"
-  location            = "East US"
+  location            = var.location
   address_space       = ["10.0.0.0/16"]
+}
+
+variable location {
+  type        = string
+  # default     = "East US"
 }
 
